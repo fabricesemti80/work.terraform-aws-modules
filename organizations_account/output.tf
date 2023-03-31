8ebe3bed4840893e217e4ef2c701286086640683
Fabrice Semti <fsemti@sportinggroup.co.uk>
2023-03-31 17:21:00 +0100

    🚀 feat(terraform): add AWS Organizations account creation module This commit adds a new Terraform module for creating AWS Organizations accounts. The module includes a resource block for creating an AWS Organizations account and an output block for the variables required for the account creation. The IAM role module has been moved to a new directory named organizations_account.

---
+++ organizations_account/output.tf
@@ -0,0 +1,12 @@
+variable "name" {
+    type = string
+    description = "Name of the new account"
+}
+variable "email" {
+    type = string
+    description = "Email for the new account"
+}
+variable "role" {
+    type = string
+    description = "Role for the new account"
+}
