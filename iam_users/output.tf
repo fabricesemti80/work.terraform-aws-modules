6780672948c4668c5257cb3ffc3037e4232fe081
Fabrice Semti <fsemti@sportinggroup.co.uk>
2023-04-03 14:01:53 +0100

    🔍 chore(iam_users): add outputs for IAM user names, ARNs, and unique IDs This commit adds outputs for IAM user names, ARNs, and unique IDs to the `output.tf` file. These outputs allow for easier access to the IAM user information generated by the `iam-user` module.

--- iam_users/output.tf
+++ iam_users/output.tf
@@ -1,5 +1,19 @@
 #? https://github.com/terraform-aws-modules/terraform-aws-iam/tree/master/modules/iam-user#outputs
 
+
+output "iam_user_names" {
+  value = values(module.iam_user).*.iam_user_name
+}
+
+output "iam_user_arns" {
+  value = values(module.iam_user).*.iam_user_arn
+}
+
+output "iam_user_unique_ids" {
+    value = values(module.iam_user).*.iam_user_unique_id
+}
+
+
 output "iam_user_summary" {
   value = concat(
     formatlist(
@@ -8,4 +22,21 @@ output "iam_user_summary" {
       [for user in module.iam_user : user.keybase_password_decrypt_command],
     )
   )
+}
+
+
+/* output "user_names" {
+  value = keys(module.iam_user)
+} */
+
+/* output "iam_user_summary" {
+  value = formatlist(
+    "To return password of user %s run: %s",
+    [for user in module.iam_user : user.iam_user_name],
+    [for user in module.iam_user : user.keybase_password_decrypt_command],
+  )
+} */
+
+output "iam_user_summary_map" {
+  value = zipmap([for user in module.iam_user : user.iam_user_name], [for user in module.iam_user : user.keybase_password_decrypt_command])
 }
