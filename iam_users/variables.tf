61b2eae63f0f42864594d75933503eb9204db076
Fabrice Semti <fsemti@sportinggroup.co.uk>
2023-04-03 15:59:19 +0100

    🔧 chore(iam_users): add permissions_boundary field to IAM user variables The permissions_boundary field has been added to the IAM user variables. This field allows for the specification of a permissions boundary for the IAM user. The default value for this field is an empty string.

--- iam_users/variables.tf
+++ iam_users/variables.tf
@@ -1,22 +1,23 @@
-
-
 variable "iam_users" {
   description = "List of IAM users to create"
   type = map(object({
     force_destroy           = bool
     pgp_key                 = string
     password_reset_required = bool
+    permissions_boundary = string
   }))
   default = {
     user1 = {
       force_destroy           = false
       pgp_key                 = "keybase:youruser"
       password_reset_required = true
+      permissions_boundary = ""
     },
     user2 = {
       force_destroy           = false
       pgp_key                 = "keybase:youruser"
       password_reset_required = true
+      permissions_boundary = ""
     }
   }
 }
