22f76381c36a7debb35561c2f3807b887a70c09f
Fabrice Semti <fsemti@sportinggroup.co.uk>
2023-04-03 14:30:18 +0100

    🔥 refactor(iam_users): remove unused output and commented code The output `iam_user_summary_map` is no longer used and the commented code is not needed. This commit removes them to improve code readability and maintainability.

--- iam_users/output.tf
+++ iam_users/output.tf
@@ -1,20 +1,17 @@
 #? https://github.com/terraform-aws-modules/terraform-aws-iam/tree/master/modules/iam-user#outputs
-
-
 output "iam_user_names" {
   value = values(module.iam_user).*.iam_user_name
 }
-
 output "iam_user_arns" {
   value = values(module.iam_user).*.iam_user_arn
 }
-
 output "iam_user_unique_ids" {
     value = values(module.iam_user).*.iam_user_unique_id
 }
-
-
-output "iam_user_summary" {
+output "user_password_retriewal_command_list" {
+  value = zipmap([for user in module.iam_user : user.iam_user_name], [for user in module.iam_user : user.keybase_password_decrypt_command])
+}
+/* output "iam_user_summary" {
   value = concat(
     formatlist(
       "To return password of user %s run: %s",
@@ -22,13 +19,10 @@ output "iam_user_summary" {
       [for user in module.iam_user : user.keybase_password_decrypt_command],
     )
   )
-}
-
-
+} */
 /* output "user_names" {
   value = keys(module.iam_user)
 } */
-
 /* output "iam_user_summary" {
   value = formatlist(
     "To return password of user %s run: %s",
@@ -36,7 +30,3 @@ output "iam_user_summary" {
     [for user in module.iam_user : user.keybase_password_decrypt_command],
   )
 } */
-
-output "iam_user_summary_map" {
-  value = zipmap([for user in module.iam_user : user.iam_user_name], [for user in module.iam_user : user.keybase_password_decrypt_command])
-}
