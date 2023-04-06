173a115d3799cdab549a253765921cd816e91fd3
Fabrice Semti <fsemti@sportinggroup.co.uk>
2023-04-06 16:08:15 +0100

    🔥 refactor(outputs.tf): remove organizational_unit_ids output The organizational_unit_ids output is no longer needed as it is not being used in the codebase. Removing it simplifies the code and reduces clutter.

--- organizations_units_accounts/outputs.tf
+++ organizations_units_accounts/outputs.tf
@@ -2,13 +2,6 @@ output "organizational_units" {
   value = local.all_ou_attributes
 }
 
-output "organizational_unit_ids" {
-  value = [
-    for unit in aws_organizations_organizational_unit.ou : unit.id
-  ]
-}
-
-
 output "account_ids" {
   value = [for account in aws_organizations_account.account : account.id]
 }
