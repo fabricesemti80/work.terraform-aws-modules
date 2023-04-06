fb4d51e1b3f9465ca9dfaa7a90850cdf7b1e2e3c
Fabrice Semti <fsemti@sportinggroup.co.uk>
2023-04-06 16:14:42 +0100

    🚀 feat(outputs.tf): add level 1 and level 2 organizational unit ids to outputs This commit adds two new outputs to the Terraform file outputs.tf. The new outputs are level_1_ou_ids and level_2_ou_ids, which contain the ids of the level 1 and level 2 organizational units respectively. These outputs will be useful for other modules that depend on this module and need to reference these organizational units.

--- organizations_units_accounts/outputs.tf
+++ organizations_units_accounts/outputs.tf
@@ -5,3 +5,11 @@ output "organizational_units" {
 output "account_ids" {
   value = [for account in aws_organizations_account.account : account.id]
 }
+
+output "level_1_ou_ids" {
+  value = [for ou in aws_organizations_organizational_unit.level_1_ous : ou.id]
+}
+
+output "level_2_ou_ids" {
+  value = [for ou in aws_organizations_organizational_unit.level_2_ous : ou.id]
+}
