8dfd64c622a8dee409c2f69bec4abfd61c00e3c8
Fabrice Semti <fsemti@sportinggroup.co.uk>
2023-04-06 16:21:27 +0100

    🔀 chore(outputs.tf): reorder output blocks The order of the output blocks has been changed to group the organizational unit IDs together and the account IDs together. This improves readability and makes it easier to find the desired output.

--- organizations_units_accounts/outputs.tf
+++ organizations_units_accounts/outputs.tf
@@ -2,14 +2,14 @@ output "organizational_units" {
   value = local.all_ou_attributes
 }
 
-output "account_ids" {
-  value = [for account in aws_organizations_account.account : account.id]
-}
-
 output "level_1_ou_ids" {
   value = [for ou in aws_organizations_organizational_unit.level_1_ous : ou.id]
 }
 
 output "level_2_ou_ids" {
   value = [for ou in aws_organizations_organizational_unit.level_2_ous : ou.id]
+}
+
+output "account_ids" {
+  value = [for account in aws_organizations_account.account : account.id]
 }
